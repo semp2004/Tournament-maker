56f4ad31723b1718fdad7582a650eae0d63a2260
Čapčuch, Patrik - Student Rijn IJssel <00317034@rijnijssel.nl>
2023-06-06 13:57:10 +0200

    feat(backend) Full Path

--- app/Http/Controllers/StadiumController.php
+++ app/Http/Controllers/StadiumController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Stadium;
 use Illuminate\Http\Request;
-use Storage;
+use Illuminate\Support\Facades\Storage;
 
 class StadiumController extends Controller
 {
