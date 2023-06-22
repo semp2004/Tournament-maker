3cd7ee397c3b64eb1286d60ec1c744cbc3744ab8
Sem Peters <00303712@rijnijssel.nl>
2023-06-22 08:45:49 +0200

    added layout for giving in scores to the teams

--- resources/views/tournament/tournament.blade.php
+++ resources/views/tournament/tournament.blade.php
@@ -5,16 +5,57 @@
     <br>
     <div class="tournament-bracket">
         @foreach ($tournament->matches as $match)
-            <div class="pl-96 pr-96 text-center">
-                <h2 class="mt-4 text-2xl">Round</h2>
+            <div class="pl-60 pr-60 text-center">
+                <h2 class="mt-4 text-2xl">Round {{$match->round}}</h2>
                 <div class="p-4 bg-gray-800 rounded-md">
-                    <div>
-                        <span class="font-bold text-2xl">{{ $match->team1?->name ?? 'not defined' }}</span>
-                        <span class="text-xl text-red-600">vs</span>
-                        <span class="font-bold text-2xl">{{ $match->team2?->name ?? 'not defined' }}</span>
+                    <div class="flex items-center justify-between">
+                        <div class="mr-2">
+                            @if($match->team_1_score == "0")
+                                <form method="get">
+                                    @csrf
+                                    <input type="number" id="score_team_1" name="score_team_1" min="1"
+                                           class="p-2 border rounded-md bg-gray-800">
+                                    <button type="submit" formaction="{{ route('tournament.score1', [
+        'tournament' => $match,
+        'match_id' => $match->id,
+        'team_id' => $match->team_1_score ?? $match->team1->id,
+    ]) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                        Submit
+                                    </button>
+                                </form>
+                            @else
+{{--                                @dd($match->team_1_score, $match->team1->id, $match->team_1_id)--}}
+
+                                <span class="font-bold text-2xl">{{ $match->team_1_score }}</span>
+                            @endif
+                        </div>
+                        <div class="flex text-center">
+                            <span class="font-bold text-2xl">{{ $match->team1?->name ?? 'not defined' }}</span>
+                            <span class="text-xl text-red-600 mx-2">vs</span>
+                            <span class="font-bold text-2xl">{{ $match->team2?->name ?? 'not defined' }}</span>
+                        </div>
+                        <div class="flex items-center">
+                            <div>
+                                @if($match->team_2_score == "0")
+                                    <form method="get">
+                                        @csrf
+                                        <input type="number" id="score_team_2" name="score_team_2" min="1"
+                                               class="p-2 border rounded-md bg-gray-800">
+                                        <button type="submit"
+                                                class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                            Submit
+                                        </button>
+                                    </form>
+                                @else
+                                    <span class="font-bold text-2xl">{{ $match->team_2_score }}</span>
+                                @endif
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
+
         @endforeach
+        <div class="border-t border-gray-300"></div>
     </div>
 @endsection
